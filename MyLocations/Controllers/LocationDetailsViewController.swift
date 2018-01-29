db85b82d32775118e17c490c9d12b7e0495a0771
Claudia Carrillo <ccarrillo@nearsoft.com>
2018-01-29 15:06:41 -0600

    Fix category picker

--- MyLocations/Controllers/LocationDetailsViewController.swift
+++ MyLocations/Controllers/LocationDetailsViewController.swift
@@ -19,6 +19,7 @@ class LocationDetailsViewController: UITableViewController {
   
   var coordinate = CLLocationCoordinate2D(latitude: 0, longitude: 0)
   var placemark: CLPlacemark?
+  var categoryName = "No Category"
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -26,6 +27,7 @@ class LocationDetailsViewController: UITableViewController {
     categoryLabel.text = ""
     latitudeLabel.text = String(format: "%2.f", coordinate.latitude)
     longitudeLabel.text = String(format: "%2.f", coordinate.longitude)
+    categoryLabel.text = categoryName
     
     if let placemark = placemark {
       addressLabel.text = string(from: placemark)
@@ -35,11 +37,31 @@ class LocationDetailsViewController: UITableViewController {
     }
     
     dateLabel.text = Date().toString(withFormat: "MMMM, dd YYYY")
+    
+    let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+    gestureRecognizer.cancelsTouchesInView = false
+    tableView.addGestureRecognizer(gestureRecognizer)
+  }
+  
+  @objc func hideKeyboard(_ gestureRecognizer: UIGestureRecognizer) {
+    let point = gestureRecognizer.location(in: tableView)
+    let indexPath = tableView.indexPathForRow(at: point)
+    if indexPath != nil && indexPath!.section == 0 && indexPath!.row == 0 {
+      return
+    }
+    descriptionTextView.resignFirstResponder()
   }
   
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
     navigationController?.isNavigationBarHidden = false
+  }
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if segue.identifier == "PickCategory" {
+      let controller = segue.destination as! CategoryPickerViewController
+      controller.selectedCategoryName = categoryName
+    }
   }
   
   @IBAction private func done() {
@@ -48,6 +70,12 @@ class LocationDetailsViewController: UITableViewController {
   
   @IBAction private func cancel() {
     navigationController?.popViewController(animated: true)
+  }
+  
+  @IBAction func categoryPickerDidPickCategory(_ segue: UIStoryboardSegue) {
+    let controller = segue.source as! CategoryPickerViewController
+    categoryName = controller.selectedCategoryName
+    categoryLabel.text = categoryName
   }
   
   // MARK: - Private methods
@@ -69,15 +97,29 @@ class LocationDetailsViewController: UITableViewController {
     if let s = placemark.country {
       text += s }
     return text
-    
   }
 }
 
 // MARK: - Table View Delegates
 extension LocationDetailsViewController {
+  override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+    if indexPath.section == 0 || indexPath.section == 1 {
+      return indexPath
+    }
+    else {
+      return nil
+    }
+  }
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    if indexPath.section == 0 && indexPath.row == 0 {
+      descriptionTextView.becomeFirstResponder()
+    }
+  }
+  
   override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
     if indexPath.section == 0 && indexPath.row == 0 {
-     return 88
+      return 88
     }
     else if indexPath.section == 2 && indexPath.row == 2 {
       addressLabel.frame.size = CGSize( width: view.bounds.size.width - 120, height: 10000)
